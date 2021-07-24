9c17b95340d21539344d5f5883e3785536116a42
Nirav Virpara <nirav.virpara3@gmail.com>
2021-07-24 12:10:51 +0530

    Initial Commit

---
+++ StockMarketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockMarketApp
+//
+//  Created by Nirav virpara on 24/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
